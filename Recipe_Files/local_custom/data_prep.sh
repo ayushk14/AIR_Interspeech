053fe719d84f3dd2a62d01df25cdc86cb910082c
Ayush Khopkar <khopkar.ayush10@gmail.com>
2019-04-03 01:28:00 +0530

    fixed path error

--- Recipe_Files/local_custom/data_prep.sh
+++ Recipe_Files/local_custom/data_prep.sh
@@ -10,6 +10,8 @@ AIR_recipe_dir=`pwd`
 data_dir=data/local/data
 dict_dir=data/local/dict
 
+mkdir -p $dict_dir
+
 python3 Python_Files/kaldi_file_preparation.py $AIR_dataset_dir $AIR_recipe_dir $data_dir $dict_dir
 
 echo "Data preparation done successfully!!!"
